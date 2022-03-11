47755b11d1d000c690c9914f2f6652d0d0a177ec
bibek raut <rautbibek47@gmail.com>
2022-03-11 21:30:42 +0545

    new changes

--- app/Http/Resources/Public/ProductResource.php
+++ app/Http/Resources/Public/ProductResource.php
@@ -22,6 +22,7 @@ class ProductResource extends JsonResource
             'short_desc' => $this->short_description,
             'slug' => $this->slug,
             'discount_type' => $this->discount_type,
+            'has_color'=> $this->has_color,
             'discount' => $this->discount,
             'cover_image' => $this->cover?MediaHelper::getThumbnailUrl($this->cover,'thumb'):asset('/images/no-image.png'),
             'variant' => $this->variant?ProductVariant::collection($this->variant):[],
